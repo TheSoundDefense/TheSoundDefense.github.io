aceb84a53d42a15b1f0b079a63427e056335f289
Jim-PC\Jim <thesounddefense@gmail.com>
2017-07-31 01:04:53 -0700

    Adding a function to turn multiples on or off

--- numboard/numberBoardCtrl.js
+++ numboard/numberBoardCtrl.js
@@ -1,6 +1,9 @@
 var numberBoardCtrl = function numberBoardCtrl() {
   var self = this;
 
+  self.multipleToggle = "on";
+  self.multiple = 5;
+
   self.on = [];
   for (var j = 0; j < 2500; j++) {
     self.on[j] = true;
@@ -17,5 +20,15 @@ var numberBoardCtrl = function numberBoardCtrl() {
 
   self.toggle = function toggle(n) {
     self.on[n] = !self.on[n];
+  }
+
+  self.toggleMultiples = function toggleMultiples() {
+    for (var k = (self.multiple-1); k < 2500; k += self.multiple) {
+      if (self.multipleToggle === "on") {
+        self.on[k] = true;
+      } else {
+        self.on[k] = false;
+      }
+    }
   }
 };
