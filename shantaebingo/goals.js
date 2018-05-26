ecd2c7ad0457d0bea1fd9b098dd65a9ce45601ba
Jim-PC\Jim <thesounddefense@gmail.com>
2018-05-25 20:27:42 -0700

    Reworking the goal

--- shantaebingo/goals.js
+++ shantaebingo/goals.js
@@ -1,192 +1,183 @@
 bingoList = [];
 bingoList[0] = [];
 bingoList[1] = [
-  {name: "1 Cacklebat (Spiderweb Island)", types: ["Cacklebats"]},
-  {name: "2 Heart Squids (Saliva Island)", types: ["Heart_Squid"]},
-  {name: "Map (Spittle Maze)", types: ["Map"]},
-  {name: "1 Cacklebat (Frostbite Island)", types: ["Cacklebats"]},
-  {name: "Ring (Saliva Island)", types: []},
-  {name: "Map (Oubliette of Suffering)", types: ["Map"]}
+  {name: "Keep Death Mask", types: ["Keep_Item"]},
+  {name: "Kill 15 wolves (Frostbite Island)", types: ["Kill_Quest"]},
+  {name: "No crouching (Steel Maggot)", types: ["Boss_Restriction","Maggot_Restriction"]},
+  {name: "Pirate Flare out (Lonely Grave)", types: []},
+  {name: "Map (Oubliette of Suffering)", types: ["Map"]},
 ];
 bingoList[2] = [
-  {name: "2 Heart Squids (Lost Catacombs)", types: ["Heart_Squid"]},
-  {name: "No Risky Shuffle (Scuttle Town)", types: ["Move_Restriction"]},
-  {name: "No Risky Shuffle (Frostbite Island)", types: ["Move_Restriction"]},
-  {name: "1 Cacklebat (Saliva Island)", types: ["Cacklebats"]},
-  {name: "2 Heart Squids (Spiderweb Island)", types: ["Heart_Squid"]}
+  {name: "Kill 15 guards (Scuttle Town)", types: ["Kill_Quest"]},
+  {name: "2 Heart Squids (Saliva Island)", types: ["Heart_Squid"]},
+  {name: "Enter every normal room (Ye Royal Sewers)", types: []},
+  {name: "No Bubble Shield (Pirate Master)", types: ["Boss_Restriction","Item_Restriction","Master_Restriction"]},
+  {name: "No Risky Shuffle (Abandoned Factory)", types: ["Move_Restriction"]},
 ];
 bingoList[3] = [
-  {name: "No Risky Shuffle (Tan Line Island)", types: ["Move_Restriction"]},
-  {name: "Get 1 key (Spittle Maze)", types: ["Get_Keys"]},
-  {name: "No Risky Shuffle (Saliva Island)", types: ["Move_Restriction"]},
-  {name: "No Risky Shuffle (Spiderweb Island)", types: ["Move_Restriction"]},
-  {name: "Map (Cackle Tower)", types: ["Map"]},
-  {name: "2 Cacklebats (Scuttle Town)", types: ["Cacklebats"]}
+  {name: "Ring (Saliva Island)", types: []},
+  {name: "2 Heart Squids (Spiderweb Island)", types: ["Heart_Squid"]},
+  {name: "Kill 6 Mermaids (Saliva Island)", types: ["Kill_Quest"]},
+  {name: "1 Cacklebat (Spiderweb Island)", types: ["Cacklebats"]},
+  {name: "No Risky Shuffle in crusher room (Lost Catacombs)", types: ["Move_Restriction"]},
 ];
 bingoList[4] = [
-  {name: "Kill 9 guards (Scuttle Town)", types: ["Kill_Quest"]},
-  {name: "2 Heart Squids (Abandoned Factory)", types: ["Heart_Squid"]},
-  {name: "Cannon maze Heart Squid (Frostbite Island)", types: ["Heart_Squid"]},
-  {name: "1 Cacklebat (Mud Bog Island)", types: ["Cacklebats"]},
-  {name: "2 Heart Squids (Spittle Maze)", types: ["Heart_Squid"]}
+  {name: "1 Cacklebat (Saliva Island)", types: ["Cacklebats"]},
+  {name: "2 Heart Squids (Spittle Maze)", types: ["Heart_Squid"]},
+  {name: "Map (Cackle Tower)", types: ["Map"]},
+  {name: "1 Cacklebat (Frostbite/Propeller Town)", types: ["Cacklebats"]},
+  {name: "No Risky Shuffle (Mud Bog Island)", types: ["Move_Restriction"]},
 ];
 bingoList[5] = [
-  {name: "Complete 2 gauntlets (Spittle Maze)", types: ["Gauntlets"]},
-  {name: "3 Heart Squids (Scuttle Town)", types: ["Heart_Squid"]},
-  {name: "Map (Lost Catacombs)", types: ["Map"]},
-  {name: "Get 1 key (Oubliette of Suffering)", types: ["Get_Keys"]},
-  {name: "1 Cacklebat (Tan Line Island)", types: ["Cacklebats"]},
-  {name: "2 Cacklebats (Spiderweb Island)", types: ["Cacklebats"]}
+  {name: "(Super) Pike Ball damage only (Empress Spider)", types: ["Boss_Restriction","Spider_Restriction"]},
+  {name: "Kill 4 bosses without advance Risky items", types: ["Boss_Restriction","Risky_Item"]},
+  {name: "No Risky's Scimitar (Cyclops Plant)", types: ["Boss_Restriction","Cyclops_Restriction","Risky_Item"]},
+  {name: "2 Heart Squids (Lost Catacombs)", types: ["Heart_Squid"]},
+  {name: "Map (Spittle Maze)", types: ["Map"]},
 ];
 bingoList[6] = [
-  {name: "No crouching (Steel Maggot)", types: ["Boss_Restriction","Maggot_Restriction"]},
-  {name: "No Risky Shuffle (Cackle Tower)", types: ["Move_Restriction"]},
-  {name: "2 Cacklebats (Saliva Island)", types: ["Cacklebats"]},
-  {name: "4 Heart Squids (Scuttle Town)", types: ["Heart_Squid"]},
-  {name: "5 distinct save men", types: ["Save_Man"]}
+  {name: "No Risky Shuffle (Propeller Town)", types: ["Move_Restriction"]},
+  {name: "2 Cacklebats (Spiderweb Island)", types: ["Cacklebats"]},
+  {name: "1 Cacklebat (Mud Bog Island/Village)", types: ["Cacklebats"]},
+  {name: "Kill 5 Scorpion Girls (Tan Line Island)", types: ["Kill_Quest"]},
+  {name: "3 Heart Squids (Scuttle Town)", types: ["Heart_Squid"]},
+  {name: "No Pirate Flares (Scuttle Town)", types: ["Item_Restriction"]},
 ];
 bingoList[7] = [
-  {name: "Kill 15 wolves (Frostbite Island)", types: ["Kill_Quest"]},
-  {name: "Map (Abandoned Factory)", types: ["Map"]},
-  {name: "Enter every normal room (Ye Royal Sewers)", types: []},
-  {name: "No Risky Shuffle (Lost Catacombs)", types: ["Move_Restriction"]},
-  {name: "No Risky Shuffle (Abandoned Factory)", types: ["Move_Restriction"]}
+  {name: "4 Heart Squids (Scuttle Town)", types: ["Heart_Squid"]},
+  {name: "3 Heart Squids (Mud Bog Island/Village)", types: ["Heart_Squid"]},
+  {name: "5 distinct save men", types: ["Save_Man"]},
+  {name: "No Risky's Cannon (Dagron)", types: ["Boss_Restriction","Dagron_Restriction","Risky_Item"]},
+  {name: "2 Heart Squids (Abandoned Factory)", types: ["Heart_Squid"]},
 ];
 bingoList[8] = [
-  {name: "Kill 15 skeletons (Cackle Tower)", types: ["Kill_Quest"]},
-  {name: "Kill 20 zombie bodies (Spiderweb Island)", types: ["Kill_Quest"]},
   {name: "First-level hair upgrades", types: ["Upgrade","Shop"]},
-  {name: "Complete 1 gauntlet (Oubliette of Suffering)", types: ["Gauntlets"]},
-  {name: "3 Heart Squids (Mud Bog Island)", types: ["Heart_Squid"]}
-];
-bingoList[9] = [
-  {name: "Kill 5 Scorpion Girls (Tan Line Island)", types: ["Kill_Quest"]},
   {name: "Learn Backdash", types: ["Upgrade","Shop"]},
   {name: "1 Pistol Tune-Up", types: ["Upgrade","Shop"]},
-  {name: "2 Cacklebats (Frostbite Island)", types: ["Cacklebats"]},
-  {name: "Kill 6 Mermaids (Saliva Island)", types: ["Kill_Quest"]}
+  {name: "No Pirate Flares (Saliva Island)", types: ["Item_Restriction"]},
+  {name: "Kill 15 skeletons (Cackle Tower)", types: ["Kill_Quest"]},
+];
+bingoList[9] = [
+  {name: "Keep Library Card", types: ["Keep_Item"]},
+  {name: "2 Heart Squids (Cackle Tower)", types: ["Heart_Squid"]},
+  {name: "1 Cacklebat (Tan Line Island)", types: ["Cacklebats"]},
+  {name: "Kill 7 Scorpion Girls (Tan Line Island)", types: ["Kill_Quest"]},
+  {name: "Step on five conveyor belts (Abandoned Factory)", types: ["Factory_Loop"]},
 ];
 bingoList[10] = [
-  {name: "Complete 2 gauntlets (Cackle Tower)", types: ["Gauntlets"]},
-  {name: "Get 2 keys (Lost Catacombs)", types: ["Get_Keys"]},
-  {name: "Get 3 keys (Cackle Tower)", types: ["Get_Keys"]},
-  {name: "No Risky's Cannon (Dagron)", types: ["Boss_Restriction","Dagron_Restriction"]},
-  {name: "2 Heart Squids (Cackle Tower)", types: ["Heart_Squid"]},
-  {name: "No Pirate Flares (Scuttle Town)", types: ["Item_Restriction"]}
+  {name: "Complete 4 gauntlets", types: ["Gauntlets","Low_Gauntlets"]},
+  {name: "End with 200 gems", types: ["Gem_Count"]},
+  {name: "2 Cacklebats (Frostbite/Propeller Town)", types: ["Cacklebats"]},
+  {name: "Cannon maze Heart Squid (Propeller Town)", types: ["Heart_Squid"]},
+  {name: "2 Cacklebats (Scuttle Town)", types: ["Cacklebats"]},
+  {name: "No Pirate Flares (Spiderweb Island)", types: ["Item_Restriction"]},
 ];
 bingoList[11] = [
-  {name: "No Risky's Scimitar (Cyclops Plant)", types: ["Boss_Restriction","Cyclops_Restriction"]},
-  {name: "8 distinct save men", types: ["Save_Man"]},
+  {name: "Map (Abandoned Factory)", types: ["Map"]},
+  {name: "No hair whips (Squid Baron)", types: ["Boss_Restriction","Squid_Restriction"]},
+  {name: "No Drizzle Fountain (Spittle Maze)", types: ["Drizzle_Fountain"]},
   {name: "Learn Recover", types: ["Upgrade","Shop"]},
-  {name: "2 Cacklebats (Tan Line Island)", types: ["Cacklebats"]},
-  {name: "2 Cacklebats (Mud Bog Island)", types: ["Cacklebats"]},
-  {name: "3 Cacklebats (Scuttle Town)", types: ["Cacklebats"]}
+  {name: "2 Cacklebats (Saliva Island)", types: ["Cacklebats"]},
 ];
 bingoList[12] = [
-  {name: "Kill 10 archers (Village of Lost Souls)", types: ["Kill_Quest"]},
-  {name: "Kill 7 Scorpion Girls (Tan Line Island)", types: ["Kill_Quest"]},
-  {name: "Keep Death Mask", types: ["Keep_Item"]},
-  {name: "Kill 10 muck enemies (Mud Bog Island)", types: ["Kill_Quest"]},
-  {name: "No hair whips (Squid Baron)", types: ["Boss_Restriction","Squid_Restriction"]}
+  {name: "Kill 5 bosses without advance Risky items", types: ["Boss_Restriction","Risky_Item"]},
+  {name: "No Pirate Flares (Mud Bog Island)", types: ["Item_Restriction"]},
+  {name: "(Super) Pike Ball damage only (Cyclops Plant)", types: ["Boss_Restriction","Cyclops_Restriction"]},
+  {name: "Kill 20 zombie bodies (Spiderweb Island)", types: ["Kill_Quest"]},
+  {name: "Get 1 key (Spittle Maze)", types: ["Get_Keys","Spittle_Gauntlets"]},
 ];
 bingoList[13] = [
-  {name: "No Pirate Flares (Saliva Island)", types: ["Item_Restriction"]},
-  {name: "No Super Pike Ball (Empress Spider)", types: ["Boss_Restriction","Spider_Restriction"]},
-  {name: "Pirate Flare out (Lonely Grave)", types: []},
-  {name: "No Risky Shuffle (Mud Bog Island)", types: ["Move_Restriction"]},
-  {name: "Heal 5 full hearts in the bath house", types: ["Heal_Hearts"]},
-  {name: "Scimitar Polish", types: ["Upgrade","Shop"]}
+  {name: "2 Cacklebats (Mud Bog Island/Village)", types: ["Cacklebats"]},
+  {name: "Map (Lost Catacombs)", types: ["Map"]},
+  {name: "Kill 10 archers (Village of Lost Souls)", types: ["Kill_Quest"]},
+  {name: "8 distinct save men", types: ["Save_Man"]},
+  {name: "4 Dark Magic", types: ["Dark_Magic","Low_Magic"]},
 ];
 bingoList[14] = [
-  {name: "1 trade with the Mayor", types: ["Mayor_Trade"]},
-  {name: "No Pirate Flares (Frostbite Island)", types: ["Item_Restriction"]},
-  {name: "Step on five conveyor belts (Abandoned Factory)", types: []},
-  {name: "Get 2 keys (Oubliette of Suffering)", types: ["Get_Keys"]},
-  {name: "2 Heart Squids (Oubliette of Suffering)", types: ["Heart_Squid"]},
-  {name: "Get 4 keys (Abandoned Factory)", types: ["Get_Keys"]}
+  {name: "Kill 8 Nagas (Village of Lost Souls)", types: ["Kill_Quest"]},
+  {name: "(Super) Pike Ball damage only (Pirate Master)", types: ["Boss_Restriction","Master_Restriction"]},
+  {name: "Get 4 keys (Abandoned Factory)", types: ["Get_Keys"]},
+  {name: "Scimitar Polish", types: ["Upgrade","Shop"]},
+  {name: "1 Pirate Master trial", types: ["Trials"]},
 ];
 bingoList[15] = [
-  {name: "Kill 2 pre-Frostbite bosses without advance Risky items", types: ["Boss_Restriction","Risky_Item"]},
-  {name: "No Bubble Shield (Pirate Master)", types: ["Item_Restriction","Master_Restriction"]},
-  {name: "Complete 2 gauntlets (Oubliette of Suffering)", types: ["Gauntlets"]},
-  {name: "4 Dark Magic", types: ["Dark_Magic"]},
-  {name: "3 Heart Squids (Frostbite Island)", types: ["Heart_Squid"]}
+  {name: "Complete 1 gauntlet (Oubliette of Suffering)", types: ["Gauntlets","Oubliette_Gauntlets"]},
+  {name: "2 Pistol Tune-Ups", types: ["Upgrade","Shop"]},
+  {name: "Heal 3 full hearts in the bath house", types: ["Heal_Hearts"]},
+  {name: "End with 300 gems", types: ["Gem_Count"]},
+  {name: "No Pirate Flares (Tan Line Island)", types: ["Item_Restriction"]},
 ];
 bingoList[16] = [
-  {name: "1 Heart Squid (Tan Line Temple)", types: ["Heart_Squid"]},
-  {name: "No Pirate Flares (Spiderweb Island)", types: ["Item_Restriction"]},
-  {name: "No Pirate Flares (Tan Line Island)", types: ["Item_Restriction"]},
-  {name: "Unlock the ground floor (Lost Catacombs)", types: []},
-  {name: "Complete 4 gauntlets", types: ["Gauntlets"]},
-  {name: "Kill 2 bosses with no upgrades", types: ["Boss_Restriction","No_Powerups"]}
+  {name: "Kill 10 muck enemies (Mud Bog Island)", types: ["Kill_Quest"]},
+  {name: "Get 2 keys (Lost Catacombs)", types: ["Get_Keys","Catacombs_Ground"]},
+  {name: "3 Heart Squids (Propeller Town)", types: ["Heart_Squid"]},
+  {name: "3 Cacklebats (Scuttle Town)", types: ["Cacklebats","Low_Magic"]},
+  {name: "(Super) Pike Ball damage only (Squid Baron)", types: ["Boss_Restriction","Squid_Restriction"]},
 ];
 bingoList[17] = [
-  {name: "Kill 10 Nagas (Village of Lost Souls)", types: ["Kill_Quest"]},
+  {name: "Get 2 keys (Tan Line Temple)", types: ["Get_Keys","Temple"]},
+  {name: "4 hearts", types: ["Hearts"]},
+  {name: "Kill 6 bosses without advance Risky items", types: ["Boss_Restriction","Risky_Item"]},
+  {name: "2 Cacklebats (Tan Line Island)", types: ["Cacklebats"]},
   {name: "12 distinct save men", types: ["Save_Man"]},
-  {name: "Get 3 keys (Lost Catacombs)", types: ["Get_Keys"]},
-  {name: "Second-level hair upgrades", types: ["Upgrade","Shop"]},
-  {name: "No Risky Shuffle (Propeller Town)", types: ["Move_Restriction"]},
-  {name: "No Risky Shuffle (Village of Lost Souls)", types: ["Move_Restriction"]}
+  {name: "Go to court", types: []},
 ];
 bingoList[18] = [
-  {name: "Get 5 keys (Abandoned Factory)", types: ["Get_Keys"]},
-  {name: "(Super) Pike Ball damage only (Squid Baron)", types: ["Boss_Restriction","Squid_Restriction"]},
-  {name: "400 gems", types: ["Gem_Count"]},
-  {name: "Get 2 keys (Tan Line Temple)", types: ["Get_Keys"]},
-  {name: "4 Cacklebats (Scuttle Town)", types: ["Cacklebats"]}
+  {name: "Complete 5 gauntlets", types: ["Gauntlets"]},
+  {name: "1 Heart Squid (Tan Line Temple)", types: ["Heart_Squid","Temple"]},
+  {name: "Get 5 keys (Abandoned Factory)", types: ["Get_Keys","Factory_Loop"]},
+  {name: "End with 400 gems", types: ["Gem_Count"]},
+  {name: "Get 1 key (Oubliette of Suffering)", types: ["Get_Keys","Oubliette_Gauntlets"]},
 ];
 bingoList[19] = [
-  {name: "8 Dark Magic", types: ["Dark_Magic"]},
-  {name: "5 hearts", types: ["Hearts"]},
-  {name: "500 gems", types: ["Gem_Count"]},
-  {name: "(Super) Pike Ball damage only (Cyclops Plant)", types: ["Boss_Restriction","Cyclops_Restriction"]},
-  {name: "(Super) Pike Ball damage only (Empress Spider)", types: ["Boss_Restriction","Spider_Restriction"]},
-  {name: "Complete 5 gauntlets", types: ["Gauntlets"]}
+  {name: "Second-level hair upgrades", types: ["Upgrade","Shop"]},
+  {name: "Learn Power Kick", types: ["Upgrade","Shop"]},
+  {name: "No Super Pike Ball (Empress Spider)", types: ["Boss_Restriction","Spider_Restriction"]},
+  {name: "Complete 2 gauntlets (Cackle Tower)", types: ["Gauntlets","Cackle_Gauntlets","Low_Gauntlets"]},
+  {name: "No Pirate Flares (Frostbite/Propeller Town)", types: ["Item_Restriction"]},
 ];
 bingoList[20] = [
-  {name: "Kill 3 bosses with no powerups", types: ["Boss_Restriction","No_Powerups"]},
+  {name: "Get 3 keys (Cackle Tower)", types: ["Get_Keys","Cackle_Gauntlets"]},
+  {name: "(Super) Pike Ball damage only (Steel Maggot)", types: ["Boss_Restriction","Maggot_Restriction"]},
   {name: "Start the fan (Cackle Tower)", types: []},
-  {name: "No Drizzle Fountain (Spittle Maze)", types: ["Restriction"]},
-  {name: "No Risky Shuffle (Oubliette of Suffering)", types: ["Move_Restriction"]},
-  {name: "No Pirate Flares (Mud Bog Island)", types: ["Item_Restriction"]},
-  {name: "Learn Power Kick", types: ["Learn_Move"]}
+  {name: "2 Pirate Master trials", types: ["Trials"]},
+  {name: "End with 500 gems", types: ["Gem_Count"]},
 ];
 bingoList[21] = [
-  {name: "(Super) Pike Ball damage only (Pirate Master)", types: ["Boss_Restriction","Master_Restriction"]},
-  {name: "Complete 3 gauntlets (Cackle tower)", types: ["Gauntlets"]},
-  {name: "1 Pirate Master trial", types: ["Trials"]},
-  {name: "Kill 3 pre-Frostbite bosses without advance Risky items", types: ["Boss_Restriction","Risky_Item"]},
-  {name: "Get 3 keys (Tan Line Temple)", types: ["Get_Keys"]},
-  {name: "Complete 6 gauntlets", types: ["Gauntlets"]}
+  {name: "1 trade with the Mayor", types: ["Mayor_Trade"]},
+  {name: "2 Heart Squids (Oubliette of Suffering)", types: ["Heart_Squid", "Oubliette_Circle"]},
+  {name: "Heal 6 full hearts in the bath house", types: ["Heal_Hearts"]},
+  {name: "End with 600 gems", types: ["Gem_Count"]},
+  {name: "4 Cacklebats (Scuttle Town)", types: ["Cacklebats", "Low_Magic"]},
+  {name: "5 hearts", types: ["Hearts"]},
 ];
 bingoList[22] = [
-  {name: "Complete 7 gauntlets", types: ["Gauntlets"]},
-  {name: "Secret sewer gauntlet", types: ["Gauntlets"]},
-  {name: "2 trades with the Mayor", types: ["Mayor_Trade"]},
-  {name: "Keep Library Card", types: ["Keep_Item"]},
-  {name: "(Super) Pike Ball damage only (Steel Maggot)", types: ["Boss_Restriction","Maggot_Restriction"]},
-  {name: "Heal 10 full hearts in the bath house", types: ["Heal_Hearts"]}
+  {name: "Complete 3 gauntlets (Cackle Tower)", types: ["Gauntlets", "Cackle_Gauntlets","Low_Gauntlets"]},
+  {name: "Complete 2 gauntlets (Oubliette of Suffering)", types: ["Gauntlets", "Oubliette_Gauntlets","Low_Gauntlets"]},
+  {name: "Unlock the ground floor (Lost Catacombs)", types: ["Catacombs_Ground"]},
+  {name: "8 Dark Magic", types: ["Dark_Magic"]},
+  {name: "No Risky Shuffle (Oubliette of Suffering)", types: ["Move_Restriction", "Oubliette_Circle"]},
 ];
 bingoList[23] = [
-  {name: "6 hearts", types: ["Hearts"]},
-  {name: "12 Dark Magic", types: ["Dark_Magic"]},
-  {name: "Heal 15 full hearts in the bath house", types: ["Heal_Hearts"]},
-  {name: "600 gems", types: ["Gem_Count"]},
-  {name: "2 Pirate Master trials", types: ["Trials"]},
-  {name: "Go to court", types: []}
+  {name: "Get 3 keys (Tan Line Temple)", types: ["Get_Keys", "Temple"]},
+  {name: "Get 3 keys (Lost Catacombs)", types: ["Get_Keys", "Catacombs_Ground"]},
+  {name: "3 Pirate Master trials", types: ["Trials"]},
+  {name: "Exit through the exit (Drizzle Fountain)", types: ["Drizzle_Fountain"]},
+  {name: "Kill 4 bosses with no powerups", types: ["Boss_Restriction", "No_Powerups"]},
+  {name: "Complete 6 gauntlets", types: ["Gauntlets"]},
 ];
 bingoList[24] = [
-  {name: "16 Dark Magic", types: ["Dark_Magic"]},
-  {name: "Exit through the exit (Drizzle Fountain)", types: []},
-  {name: "(Super) Pike Ball damage only (Dagron)", types: ["Boss_Restriction","Dagron_Restriction"]},
-  {name: "7 hearts", types: ["Hearts"]},
-  {name: "3 Pirate Master trials", types: ["Trials"]},
-  {name: "700 gems", types: ["Gem_Count"]}
+  {name: "Complete 7 gauntlets", types: ["Gauntlets"]},
+  {name: "Heal 9 full hearts in the bath house", types: ["Heal_Hearts"]},
+  {name: "Hold 999 gems at one time", types: ["Gem_Count"]},
+  {name: "Get 2 keys (Oubliette of Suffering)", types: ["Get_Keys"]},
+  {name: "6 hearts", types: ["Hearts"]},
 ];
 bingoList[25] = [
   {name: "No shopping", types: ["Shop"]},
-  {name: "90% completion", types: ["Percent"]},
-  {name: "20 Dark Magic", types: ["Dark_Magic"]},
-  {name: "80% completion", types: ["Percent"]},
-  {name: "70% completion", types: ["Percent"]}
+  {name: "2 trades with the Mayor", types: ["Mayor_Trade"]},
+  {name: "7 hearts", types: ["Hearts"]},
+  {name: "Secret sewer gauntlet", types: ["Gauntlets"]},
+  {name: "Kill 5 bosses with no powerups", types: ["Boss_Restriction", "No_Powerups"]},
+  {name: "12 Dark Magic", types: ["Dark_Magic"]},
 ];
